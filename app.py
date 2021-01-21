71ec01e13dbbfb754c516f73cb932a14a7bff15e
Michael Keith <michaelkeith@utah.gov>
2021-01-21 09:19:24 -0700

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ class main:
 					g.game_over()
 				else:
 					print('invalid entry, make sure your casing is correct')
-			except (game.GameError) as e:
+			except (game.GameError,IndexError) as e:
 				print(e)
 
 if __name__ == '__main__':
